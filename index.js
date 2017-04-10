d8dac0704ed14e43bd78e4d34b249cf7aab8414b
Rocky Kitamura <rockykitamura@gmail.com>
2017-04-10 15:06:59 -0700

    Change require

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function loader( source ) {
   var template = dust.compile( source, name ); 
 
   // Return the code needed to run this template
-  return "var dust = require('dustjs-linkedin');"
+  return "var dust = require('dustjs');"
       + deps.join( ' ' )
       + template
       + "var fn = " + options.wrapperGenerator( name )

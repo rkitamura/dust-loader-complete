4bd8afd30a713f61041154a6d8ae5df284bd5a9c
Rocky Kitamura <rockykitamura@gmail.com>
2017-04-10 15:24:36 -0700

    Original

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function loader( source ) {
   var template = dust.compile( source, name ); 
 
   // Return the code needed to run this template
-  return "var dust = require('dustjs/lib/dust');"
+  return "var dust = require('" + options.dustAlias + "/lib/dust'); "
       + deps.join( ' ' )
       + template
       + "var fn = " + options.wrapperGenerator( name )
